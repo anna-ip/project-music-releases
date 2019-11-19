e0fade4dbc8b6b1823d1a78737e51240b8e1f860
anna-ip <anna.almestal@gmail.com>
2019-11-19 17:03:09 +0100

    Album list shows

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,58 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from './Album'
 
 console.log(data)
-
+console.log(Album)
+/***** First try as it looked in the very beginning Technigo version *****/
+/*
 export const App = () => {
   return (
     <div>
+      <Album />
       Find me in src/app.js!
     </div>
   )
-}
+}*/
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <Album />
+        <artists />
+        Find me in src/app.js!
+      </div>
+      {data.albums.items.map(item => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
+    </div>
+  );
+};
+
+/****Second test as did in Stack Overflow***/
+/*
+export const App = () => {
+  return (
+    <div className="App">
+      {data.albums.items.forEach(item => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
+    </div>
+  );
+}; */
+
+/* **First test**
+export const App = () => {
+  return (
+    <div>
+      {data.map(() => {
+        return (
+          <Album key={Album.items.name} name={Album.items.name} />
+        );
+      })}
+      Find me in src/app.js!
+    </div>
+  )
+}*/
+
